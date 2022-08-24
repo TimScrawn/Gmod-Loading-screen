718846ca14c0c31479c9e61e7e8fe6073109bab4
Haxxor2020 <75811954+Haxxor2020@users.noreply.github.com>
2022-08-24 19:10:10 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,7 +27,7 @@ var l_serverName = "";
 // Use a video for the background?
 // Otherwise it will use images
 // 'true' or 'false'
-var l_bgVideo = false;
+var l_bgVideo = true;
 
 // Default available videos:
 // "aurora.webm"
